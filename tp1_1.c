b29100e033b4505d7a2f33f944503fa460405f87
zequivill.8338@gmail.com <ezequielvillagran65@gmail.com>
2025-03-25 16:49:01 -0300

    c modificado y .gitignore agregado

--- tp1_1.c
+++ tp1_1.c
@@ -4,7 +4,14 @@ int main(){
 
     printf("Hola mundo");
 
+    int variable = 42;
+    int *puntero = &variable;
 
+    printf("Contenido del puntero (valor apuntado): %d\n", *puntero);
+    printf("Direccion almacenada en el puntero: %p\n", puntero);
+    printf("Direccion de memoria de la variable: %p\n", &variable);
+    printf("Direccion de memoria del puntero: %p\n", &puntero);
+    printf("Tamano de memoria de la variable: %zu bytes\n", sizeof(variable));
 
     return 0;
 }
